8077e81e5864c3a116d0a6539d4abfad2e0d9ca2
Ramirez Cruz Jesus Gael <jramirezc2404@alumno.ipn.mx>
2026-03-25 00:50:16 -0600

    tengo fe en que valla bn

---
+++ Examen/Examen.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Examen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String nombre = "", apellidopaterno = "", apellidomaterno = "",fechanacimiento = "", direccion = "";
+
+        int cuartos = 0;
+
+        do { 
+            System.out.println("Menu");
+            System.out.println("1. Ingresar datos del cliente");
+            System.out.println("2. Ver tipos de piso");
+            System.out.println("3. Cuartos y medidas");
+            System.out.println("4. Comprar");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Datos del Cliente");
+                    System.out.print("Nombre: ");
+                    nombre = sc.nextLine();
+
+                    System.out.print("Apellido Paterno: ");
+                    apellidopaterno = sc.nextLine();
+
+                    System.out.print("Apellido Materno: ");
+                    apellidomaterno = sc.nextLine();
+
+                    System.out.print("Fecha de Nacimiento: ");
+                    fechanacimiento = sc.nextLine();
+
+                    System.out.print("Direccion: ");
+                    direccion = sc.nextLine();
+                    break;
+
+
+                case 2:
+                    System.out.println("Tipos de Piso");
+                    System.out.println("1. Porcelanato = $22.35 m2");
+                    System.out.println("2. Marmoleado = $34.27 m2");
+                    System.out.println("3. Acrilico = $22.94 m2");
+                    break;
+
+
+                case 3:
+                    total = 0;
+
+                    do {
+                        System.out.print("Cuantos cuartos tiene? (de 2 a 4)");
+                        cuartos = sc.nextInt();
+                    } while (cuartos <= 1 || cuartos >= 5);
+                    for (int i = 1; i <= cuartos; i++) {
+                        System.out.print(" 
+
+        } 
+    }
+}
